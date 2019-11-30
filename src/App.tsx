3bf514af77f226e17b74393d52e87007b62eb8f1
ossan-engineer <bodega1123@gmail.com>
2019-11-30 19:48:16 +0900

    Add Tweet base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,171 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton, { IconButtonProps } from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { red } from '@material-ui/core/colors';
+import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
+import RepeatOutlinedIcon from '@material-ui/icons/RepeatOutlined';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import styled, { ThemeProvider } from 'styled-components';
+import omit from 'lodash.omit';
+// import styled from 'styled-components';
 
-const App: React.FC = () => {
-  return <div className="App">TWEET</div>;
+interface EnhanceedIconButtonProps extends IconButtonProps {
+  expanded: boolean;
+}
+
+const StyledAvatar = styled(Avatar)`
+  background-color: ${red[500]};
+`;
+
+const EnhanceedIconButton = (props: EnhanceedIconButtonProps) => {
+  const filteredProps = omit(props, ['expanded']);
+  return <IconButton {...filteredProps}></IconButton>;
 };
+
+const StyledIconButton = styled(EnhanceedIconButton)`
+  transform: ${props => (props.expanded ? 'rotate(180deg)' : 'rotate(0deg)')};
+  margin-left: auto;
+  transition: ${props =>
+    props.theme.transitions.create('transform', {
+      duration: props.theme.transitions.duration.shortest,
+    })};
+`;
+
+function App() {
+  const defaultTheme = createMuiTheme();
+  const [expanded, setExpanded] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={defaultTheme}>
+        <Card>
+          <CardHeader
+            avatar={<StyledAvatar aria-label="recipe">R</StyledAvatar>}
+            action={
+              <IconButton
+                aria-label="settings"
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+          />
+          <CardContent>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Item1</MenuItem>
+              <MenuItem onClick={handleClose}>Item2</MenuItem>
+              <MenuItem onClick={handleClose}>Item3</MenuItem>
+            </Menu>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="comment">
+              <ModeCommentOutlinedIcon />
+            </IconButton>
+            <IconButton aria-label="retweet">
+              <RepeatOutlinedIcon />
+            </IconButton>
+            <IconButton aria-label="add to favorites">
+              <FavoriteBorderOutlinedIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareOutlinedIcon />
+            </IconButton>
+            <StyledIconButton
+              // className={clsx(classes.expand, {
+              //   [classes.expandOpen]: expanded,
+              // })}
+              expanded={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </StyledIconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method:</Typography>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron
+                and set aside for 10 minutes.
+              </Typography>
+              <Typography paragraph>
+                Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                skillet over medium-high heat. Add chicken, shrimp and chorizo,
+                and cook, stirring occasionally until lightly browned, 6 to 8
+                minutes. Transfer shrimp to a large plate and set aside, leaving
+                chicken and chorizo in the pan. Add pimentón, bay leaves,
+                garlic, tomatoes, onion, salt and pepper, and cook, stirring
+                often until thickened and fragrant, about 10 minutes. Add
+                saffron broth and remaining 4 1/2 cups chicken broth; bring to a
+                boil.
+              </Typography>
+              <Typography paragraph>
+                Add rice and stir very gently to distribute. Top with artichokes
+                and peppers, and cook without stirring, until most of the liquid
+                is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+                reserved shrimp and mussels, tucking them down into the rice,
+                and cook again without stirring, until mussels have opened and
+                rice is just tender, 5 to 7 minutes more. (Discard any mussels
+                that don’t open.)
+              </Typography>
+              <Typography>
+                Set aside off of the heat to let rest for 10 minutes, and then
+                serve.
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      </ThemeProvider>
+    </StylesProvider>
+  );
+}
 
 export default App;
